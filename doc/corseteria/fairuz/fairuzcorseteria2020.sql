9e2c0d4ec1ab082393b3b9bc61ec82daef01fbd1
Jose Sena <senajosee@gmail.com>
2020-07-08 14:19:43 -0300

    Archivos de fairuz

--- doc/corseteria/fairuz/fairuzcorseteria2020.sql
+++ doc/corseteria/fairuz/fairuzcorseteria2020.sql
@@ -3,13 +3,14 @@ ljul_virtuemart_product_customfields
 where 
 virtuemart_custom_id = '10' OR virtuemart_custom_id='11';
 
-update ljul_articulos_tango_exp set Description = replace(Description,'  ',' ') where marca='FAIRUZ';
+update ljul_articulos_tango_exp set Description = replace(Description,'  ',' ') where marca='fairuz';
+
 update ljul_articulos_tango_exp 
 set codigo=trim(split(Description,' ',1)),
     talle=(CASE WHEN trim(split(Description,' ',2)) <> '' then trim(split(Description,' ',2)) else 'UNICO' END),
     color=(CASE WHEN trim(split(Description,' ',3)) <> '' then trim(split(Description,' ',3)) else 'VARIOS' END)
 where 
-marca='FAIRUZ';
+marca='fairuz';
     
 select
 	trim(split(Description,' ',1)) as codigo,
