53b8784b866493b48e1b8b58722b47f882235df7
Jose Sena <senajosee@gmail.com>
2020-08-03 11:41:32 -0300

    avances en medias

---
+++ doc/medias/floyd/floyd2020.sql
@@ -0,0 +1,27 @@
+delete from 
+ljul_virtuemart_product_customfields 
+where 
+virtuemart_custom_id = '10' OR virtuemart_custom_id='11';
+
+update ljul_articulos_tango_exp set Description = replace(Description,'  ',' ') where marca='floyd';
+update ljul_articulos_tango_exp 
+set codigo=trim(split(Description,' ',1)),
+    talle=(CASE WHEN trim(split(Description,' ',2)) <> '' then trim(split(Description,' ',2)) else 'UNICO' END),
+    color=(CASE WHEN trim(split(Description,' ',3)) <> '' then trim(split(Description,' ',3)) else 'VARIOS' END)
+where 
+marca='floyd';
+    
+select
+	trim(split(Description,' ',1)) as codigo,
+	(CASE WHEN trim(split(Description,' ',2)) != '' then trim(split(Description,' ',2)) else 'UNICO' END) as talle,
+	(CASE WHEN trim(split(Description,' ',3)) != '' then trim(split(Description,' ',3)) else 'VARIOS' END) as color,
+    Description,
+    AdditionalDescription,BarCode
+    from ljul_articulos_tango_exp
+where 
+marca='floyd'
+
+select distinct(talle) from ljul_articulos_tango_exp 
+where marca='floyd'
+
+order by talle
